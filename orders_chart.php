ff21f995bbb0636ca06cee761ea472e21cac86cf
SP3C7RE <RNCFDs@gmail.com>
2019-04-16 01:25:23 +0300

    Create orders_chart.php

---
+++ orders_chart.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="https://code.highcharts.com/stock/highstock.js"></script>
+    <script src="https://code.highcharts.com/stock/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/data.js"></script>
+
+    <script src="clients_chart.js"></script>
+
+    <div id="container" style="height: 400px; min-width: 310px"></div>
+
+</head>
+<body>
+
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: Kiril Andonov
+     * Date: 11.12.17
+     * Time: 19:59
+     */
+    require_once ('orders.php');
+    ?>
+
+</body>
+</html>
